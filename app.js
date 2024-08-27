fd744477254590b0d89613b9197aaff171207e7e
Jonas Schweiger <jonas.schweiger91@gmail.com>
2024-08-27 16:45:19 +0200

    Rewrite functionality into separate function

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const PADWIDTH = 960;
+const PAD_WIDTH = 960;
+const DEFAULT_GRID_SIDE_TILE_NUMBER = 16;
 // let padWidth = getComputedStyle(container).width; // returns width of container in px
-
 const container = document.querySelector(".container");
-let gridSideTileNumber = 16;
-let gridTileNumber = gridSideTileNumber * gridSideTileNumber; 
-let tileWidth = PADWIDTH / gridSideTileNumber;
 
-for (let i = 0; i < gridTileNumber; i++) {
-    const gridTile = document.createElement("div");
-    
-    gridTile.className = "gridTile";
-    gridTile.setAttribute("style", `width: ${tileWidth}px; height: ${tileWidth}px;`);
-    gridTile.addEventListener("mouseenter", () => {
-        gridTile.className = "markedTile";
-    });
+createGrid(DEFAULT_GRID_SIDE_TILE_NUMBER);
 
-    container.appendChild(gridTile);
-}
+function createGrid(gridSideTileNumber) {
+    const gridTileNumber = gridSideTileNumber * gridSideTileNumber;
+    const tileWidth = PAD_WIDTH / gridSideTileNumber;
+
+    for (let i = 0; i < gridTileNumber; i++) {
+        const gridTile = document.createElement("div");
+        
+        gridTile.className = "gridTile";
+        gridTile.setAttribute("style", `width: ${tileWidth}px; height: ${tileWidth}px;`);
+        gridTile.addEventListener("mouseenter", () => {
+            gridTile.className = "markedTile";
+        });
+
+        container.appendChild(gridTile);
+    }
+} 
