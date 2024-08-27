0a3f220a47d4ef5cadda49226e27fa021321ba89
Jonas Schweiger <jonas.schweiger91@gmail.com>
2024-08-27 16:33:47 +0200

    Add background-color change on hovering over tile

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const container = document.querySelector(".container");
+const PADWIDTH = 960;
 // let padWidth = getComputedStyle(container).width; // returns width of container in px
+
+const container = document.querySelector(".container");
 let gridSideTileNumber = 16;
 let gridTileNumber = gridSideTileNumber * gridSideTileNumber; 
-let padWidth = 960;
-let tileWidth = padWidth / gridSideTileNumber;
+let tileWidth = PADWIDTH / gridSideTileNumber;
 
 for (let i = 0; i < gridTileNumber; i++) {
     const gridTile = document.createElement("div");
+    
     gridTile.className = "gridTile";
     gridTile.setAttribute("style", `width: ${tileWidth}px; height: ${tileWidth}px;`);
+    gridTile.addEventListener("mouseenter", () => {
+        gridTile.className = "markedTile";
+    });
+
     container.appendChild(gridTile);
-} 
+}
